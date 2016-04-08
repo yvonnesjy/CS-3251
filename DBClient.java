57727f20e13dcb042824cc3dd04aa5cf7e649f2f
ColinLuff <colin.luff@gmail.com>
2016-04-07 23:37:08 -0400

    Update DBClient.java

--- DBClient.java
+++ DBClient.java
@@ -1,8 +1,36 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.StringTokenizer;
 
-public class DBClient {
-    public static void main(String[] args) {
-        
+public class dbclientTCP {
+    public static void main(String args[]) throws IOException {
+        StringTokenizer ipPort = new StringTokenizer(args[0], ":"); //Tokenizer to split the ip and port
+        // - I know I can use String.split() but I had already done this and don't feel like redoing it
+
+        InetAddress ip = InetAddress.getByName(ipPort.nextToken()); //The IP of the server to connect to
+        int port = Integer.parseInt(ipPort.nextToken()); //The port of the server to connect to
+        String output = ""; //String to hold our request to the server
+        String input; //String to hold the server's response
+        byte[] outputData;
+        byte[] inputData;
+
+        try {
+            //Setup connection to server
+            RTPSocket clientSocket = new RTPSocket(ip, port);
+            //Create our request String
+            for (int i = 1; i < args.length; i++) {
+                output += args[i] + " ";
+            }
+            //Send the request
+            outputData = output.getBytes();
+            clientSocket.writeBytes(outputData);
+            //Read and output response, terminate connection
+            inputData = clientSocket.readBytes();
+            input = new String(inputData);
+            System.out.println("From Server: " + input);
+            clientSocket.close();
+        } catch (ConnectException refused) {
+            System.out.println("Error: could not connect to server.");
+        }
     }
 }
